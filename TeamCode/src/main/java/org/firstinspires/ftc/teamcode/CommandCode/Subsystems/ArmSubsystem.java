f6361e4e6716d8c4de3f081ddfce5455eb1abf90
strangejmaster <73493283+strangejmaster@users.noreply.github.com>
2024-02-10 13:15:00 +0000

    Added ArmSubsystem target functionallity
    Created a new method that used the Arm motor's encoders to
    1. Average their position
    2. Attempt to move to a target position using a P algorithim

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandCode/Subsystems/ArmSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandCode/Subsystems/ArmSubsystem.java
@@ -6,6 +6,8 @@ import com.arcrobotics.ftclib2.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.hardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import java.lang.Math;
+
 public class ArmSubsystem extends SubsystemBase {
     private final DcMotor mtArm1;
     private final DcMotor mtArm2;
@@ -16,6 +18,9 @@ public class ArmSubsystem extends SubsystemBase {
 
         mtArm1.setDirection(ARM.ARM1_DIR);
         mtArm1.setDirection(ARM.ARM2_DIR);
+
+        mtArm1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        mtArm2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
     public void raise() {
@@ -26,6 +31,35 @@ public class ArmSubsystem extends SubsystemBase {
     public void lower() {
         mtArm1.setPower(-ARM.ARM_SPEED);
         mtArm2.setPower(-ARM.ARM_SPEED);
+    }
+
+    public int getAvgPos() {
+        return (mtArm1.getCurrentPosition + mtArm2.getCurrentPosition) / 2;
+    }
+
+    /**
+     * Attempts to keep move the arm to a target position using a P algorithim
+     * @param targetPos The encoder position to try to reach
+     */
+    public void target(final int targetPos) {
+        int avgPos = getAvgPos();
+        int error = targetPos - avgPos;
+        float total = 0;
+
+        int proportion = ARM.Kp * error;
+        
+        // Thank you!
+        // https://stackoverflow.com/questions/16656651/does-java-have-a-clamp-function
+        if (proportion > ARM.CLAMP) {
+            proportion = ARM.CLAMP;
+        } else if (proportion < -ARM.CLAMP) {
+            proportion = -ARM.CLAMP;
+        }
+
+        total += proportion;
+
+        mtArm1.setPower(total);
+        mtArm2.setPower(total);
     }
 
     public void stop() {
