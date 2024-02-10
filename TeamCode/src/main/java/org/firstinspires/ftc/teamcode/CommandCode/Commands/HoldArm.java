39298fcdfb9a08dbdd0a41ad3b3c519b11173288
strangejmaster <73493283+strangejmaster@users.noreply.github.com>
2024-02-10 13:08:17 +0000

    CREATED FIRST COMMAND!!
    HoldArm is a command that simply trys to maintain the arm's position using a PID controller.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandCode/Commands/HoldArm.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.CommandCode.Commands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+import org.firstinspires.ftc.teamcode.CommandCode.Subsystems.*;
+
+/**
+ * This command holds the robot's arm at certain position
+*/
+public class HoldArm extends CommandBase {
+    private final Subsystems.ArmSubsystem arm;
+    private final int targetPos;
+    private final int ticks;
+    private int tickCount = 0;
+
+    /**
+     * Creates a HoldArm command
+     * @param arm The arm subsystem
+     * @param ticks The number of ticks between each correction
+     */
+    public HoldArm(Subsystems.ArmSubsystem arm, int ticks) {
+        this.arm = arm;
+        this.ticks = ticks;
+
+        addRequirements(arm);
+    }
+
+    @Override
+    public void initialize() {
+        targetPos = arm.getAvgPos();
+    }
+
+    @Override
+    public void execute() {
+        if (tickCount >= ticks) {
+            arm.target(targetPos);
+        }
+    }
+
+    @Override
+    public void end() {
+        arm.stop();
+    }
+}
