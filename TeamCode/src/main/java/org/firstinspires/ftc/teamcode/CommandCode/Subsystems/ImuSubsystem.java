cb3e0678525565da344d9dae43782f953cb21fce
strangejmaster <strangejmaster@gmail.com>
2024-02-12 18:24:22 -0600

    Replaced heading angle with Rotation2D value, might be the same, testing needed
    Signed-off-by: strangejmaster <strangejmaster@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandCode/Subsystems/ImuSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandCode/Subsystems/ImuSubsystem.java
@@ -1,32 +1,36 @@
 package org.firstinspires.ftc.teamcode.CommandCode.Subsystems;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.hardware.RevIMU;
 
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.IMU;
+//import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 import org.firstinspires.ftc.teamcode.CONFIG.CONTROL_SURFACES.INTERTIALMU;
 
 
 public class ImuSubsystem extends SubsystemBase {
-    private final IMU imu;
+    private final RevIMU imu;
+//    private final IMU imu;
 
 
     public ImuSubsystem(HardwareMap hardMap) {
-        imu = hardMap.get(IMU.class, INTERTIALMU.IMU_DEVICE);
+        imu = new RevIMU(hardMap, INTERTIALMU.IMU_DEVICE);
+        imu.init();
+        /*imu = hardMap.get(IMU.class, INTERTIALMU.IMU_DEVICE);
 
         imu.initialize(
             new IMU.Parameters(new RevHubOrientationOnRobot(
                     RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
                     RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD
             ))
-        );
+        );*/
     }
 
     public double getHeading() {
-        double heading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+        double heading = imu.getRotation2d().getDegrees(); //imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
 
         /*
         // Converts the -180 to 180 degrees into a 0 to 360 degrees value
