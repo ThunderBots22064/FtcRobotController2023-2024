894ba39f36c4cb95ecf8b79bf7440f64456d7fec
strangejmaster <strangejmaster@gmail.com>
2024-02-21 04:02:46 +0000

    Added speed data to TestMove telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandCode/Commands/TestMove.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandCode/Commands/TestMove.java
@@ -41,6 +41,8 @@ public class TestMove extends CommandBase {
         telemetry.addData("Vertical Value: ", vertical);
         telemetry.addData("Turn Value: ", turn);
 
+        telemetry.addData("Speed: ", drivetrain.getSpeed());
+
         telemetry.addData("IMU Heading Raw: ", imu.getHeadingRaw());
         telemetry.addData("IMU Heading Scaled: ", imu.getHeading());
 
