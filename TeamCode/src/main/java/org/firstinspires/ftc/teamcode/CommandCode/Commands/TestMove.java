4ea8deb541e26124bc33654675310bf54a4354cd
strangejmaster <73493283+strangejmaster@users.noreply.github.com>
2024-02-21 02:46:02 +0000

    Added a telemetry obj in TestMove for future logging

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandCode/Commands/TestMove.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandCode/Commands/TestMove.java
@@ -6,6 +6,7 @@ import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 
 import org.firstinspires.ftc.teamcode.CommandCode.Subsystems.DrivetrainSubsystem;
 import org.firstinspires.ftc.teamcode.CommandCode.Subsystems.ImuSubsystem;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 import org.firstinspires.ftc.teamcode.CONFIG.CONTROLLER;
 
@@ -13,14 +14,16 @@ public class TestMove extends CommandBase {
     private final DrivetrainSubsystem drivetrain;
     private final ImuSubsystem imu;
     private final GamepadEx pad;
+    private final Teleemtry telemetry;
     
     /**
      * Creates a new TestMove command that tests the drivetrain
      * @param pad the gamepad
      * @param drivetrain the drivetrain subsystem
      * @param imu the IMU subsystem
+     * @param telemetry the telemetry object through which logs can be sent
      */
-    public TestMove(GamepadEx pad, DrivetrainSubsystem drivetrain, ImuSubsystem imu) {
+    public TestMove(GamepadEx pad, DrivetrainSubsystem drivetrain, ImuSubsystem imu, Telemetry telemetry) {
         this.drivetrain = drivetrain;
         this.imu = imu;
         this.pad = pad;
